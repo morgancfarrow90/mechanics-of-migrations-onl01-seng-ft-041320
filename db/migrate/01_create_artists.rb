792220cef56e63c4c807db5403ba13528b97e61d
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-18 22:51:43 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -2,6 +2,11 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   
   def change
     create_table :artists do |t|
+      t.string :name 
+      t.string :genre 
+      t.integer :age 
+      t.strong :hometown 
+    end
   end
   
 end
